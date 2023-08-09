0a211e7a6e37ead7894ba9daa09707403ec0d210
Victor Machado da Silva <victor0machado@gmail.com>
2023-08-09 08:27:37 -0300

    Incluída atualização sobre scan na turma 2 de Est Dados

--- turma_2/05-scan/scan.go
+++ turma_2/05-scan/scan.go
@@ -1,16 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
 
 func main() {
-	var x int
+	var x, z int
 	var y float64
 
-	fmt.Println("Informe um número: ")
-	fmt.Scan(&x)
+	fmt.Println("Informe dois números: ")
+	fmt.Scanln(&x, &z)
 	fmt.Println(x)
+	fmt.Println(z)
 
 	fmt.Println("Informe um float: ")
-	fmt.Scan(&y)
+	fmt.Scanln(&y)
 	fmt.Println(y)
+
+	// Lendo frases inteiras
+	leitor := bufio.NewReader(os.Stdin)
+	fmt.Println("Escreva um texto:")
+	msg, _ := leitor.ReadString('\n') // aspas simples é um byte
+
+	fmt.Println(msg)
+
 }
