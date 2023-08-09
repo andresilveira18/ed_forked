58cec6140676476db295670ff59fb99ac34ba86d
Victor Machado da Silva <victor0machado@gmail.com>
2023-08-09 10:26:44 -0300

    Incluído conteúdo de aula de Est Dados

--- turma_3/05-scan/scan.go
+++ turma_3/05-scan/scan.go
@@ -1,21 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
 
 func main() {
-	var x int
+	var x, z int
 	var y float64
-	var z string
 
-	fmt.Println("Informe um valor inteiro")
-	fmt.Scan(&x)
+	fmt.Println("Informe dois inteiros")
+	fmt.Scanln(&x, &z)
 	fmt.Println(x)
+	fmt.Println(z)
 
 	fmt.Println("Informe um float")
-	fmt.Scan(&y)
+	fmt.Scanln(&y)
 	fmt.Println(y)
 
-	fmt.Println("Informe um texto")
-	fmt.Scan(&z) // pega apenas a primeira palavra, sem o espaço
-	fmt.Println(z)
+	leitor := bufio.NewReader(os.Stdin)
+	fmt.Println("Escreva uma mensagem:")
+	msg, _ := leitor.ReadString('\n')
+
+	fmt.Println(msg)
 }
